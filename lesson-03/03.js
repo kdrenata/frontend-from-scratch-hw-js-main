8a123b7ce35ea0257ae1311cff3bfd1c5587ca32
kdrenata <kd.renata09@gmail.com>
2024-10-23 22:49:31 +0200

    task 3.3 done

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,12 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(first, second, third) {
+    let max = first
+    if (second > max) {
+        max = second
+    }
+    if (third > max) {
+        max = third
+    }
+    return max
+}
