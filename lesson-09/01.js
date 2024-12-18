8863c518ff5dc4ae2076cb40045092acb301c826
kdrenata <kd.renata09@gmail.com>
2024-12-18 14:05:12 +0100

    task 9.1 done

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -23,4 +23,14 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const filter = (array, callback) => {
+  const result = []
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i]
+    if (callback(element, i)) {
+      result.push(element)
+    }
+  }
+  return result
+}
+
